e07a998a30200bc6e7002ad874ffc6dc1c2bf730
anadunning <anakpedrosa@hotmail.com>
2023-05-10 11:28:01 -0300

    Created project

---
+++ src/test/java/com/anadunning/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anadunning.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
