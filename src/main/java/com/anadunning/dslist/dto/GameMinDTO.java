efbd9490346f02f60917b6023caa3f5b622f470d
anadunning <anakpedrosa@hotmail.com>
2023-05-17 20:03:07 -0300

    Ready for homologation

--- src/main/java/com/anadunning/dslist/dto/GameMinDTO.java
+++ src/main/java/com/anadunning/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.anadunning.dslist.dto;
 
 import com.anadunning.dslist.entities.Game;
+import com.anadunning.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -19,6 +20,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getGameYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
--- src/main/java/com/anadunning/dslist/services/GameService.java
+++ src/main/java/com/anadunning/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.anadunning.dslist.dto.GameDTO;
 import com.anadunning.dslist.dto.GameMinDTO;
 import com.anadunning.dslist.entities.Game;
+import com.anadunning.dslist.projections.GameMinProjection;
 import com.anadunning.dslist.repositories.GameRepository;
 
 @Service
@@ -31,6 +32,13 @@ public class GameService {
 //		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 //		return dto;
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
 	}
 
 }
